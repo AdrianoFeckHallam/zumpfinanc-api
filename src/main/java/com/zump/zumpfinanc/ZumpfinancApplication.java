f31cccf8eed7b0bff4f9e6c7b666d3e70f9d3292
AdrianoFeckHallam <feckadriano@gmail.com>
2022-06-20 02:53:16 -0300

    Alterações para o Front-end

--- src/main/java/com/zump/zumpfinanc/ZumpfinancApplication.java
+++ src/main/java/com/zump/zumpfinanc/ZumpfinancApplication.java
@@ -2,9 +2,18 @@ package com.zump.zumpfinanc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 @SpringBootApplication
-public class ZumpfinancApplication {
+public class ZumpfinancApplication implements WebMvcConfigurer{
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZumpfinancApplication.class, args);
